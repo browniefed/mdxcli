b06c5ad9a5a64e73a164ce4cba29a0a344f74707
Jason Brown <browniefed@gmail.com>
2021-01-05 17:59:08 -0800

    Fixes

--- dist/ui.js
+++ dist/ui.js
@@ -28,13 +28,12 @@ const ink_1 = require("ink");
 const ink_text_input_1 = __importDefault(require("ink-text-input"));
 const react_1 = __importStar(require("react"));
 const ink_select_input_1 = __importDefault(require("ink-select-input"));
-const ink_spinner_1 = __importDefault(require("ink-spinner"));
-const glob_1 = __importDefault(require("glob"));
 const mdxTemplate = ({ title }) => {
     return `
 import { REACT } from "../../../technologies";
 
 export const meta = {
+	published: false,
 	title: ${JSON.stringify(title)},
 	description: "",
 	type: "TUTORIAL",
@@ -44,33 +43,6 @@ export const meta = {
 	images: []
 };
 `.trimStart();
-};
-const pageTemplate = ({ subPath }) => {
-    return `
-	import Article, { meta } from "../../content/${subPath}";
-	import Layout from "../../components/pages/layout";
-
-	const Tutorial = () => {
-		return (
-		  <Layout meta={meta}>
-			<Article />
-		  </Layout>
-		);
-	  };
-	  
-	  export default Tutorial;
-	`;
-};
-const getMDXFiles = (src) => {
-    return new Promise((resolve, reject) => {
-        glob_1.default(src + "/**/*.mdx", (err, res) => {
-            if (err) {
-                reject();
-                return;
-            }
-            resolve(res);
-        });
-    });
 };
 const slugify = (title = "") => {
     var _a, _b;
@@ -82,43 +54,15 @@ const slugify = (title = "") => {
         .replace(/\,/g, "")
         .replace(/\./g, "")) !== null && _b !== void 0 ? _b : "");
 };
-const getPathParam = (filePath, directory) => {
-    const bits = filePath
-        .replace(directory, "")
-        .split("/")
-        .filter(Boolean)
-        .reverse();
-    const [last, ...all] = bits !== null && bits !== void 0 ? bits : [];
-    //@ts-ignore
-    return [path_1.default.basename(last, path_1.default.extname(last)), ...all].reverse();
-};
 const App = () => {
     const { exit } = ink_1.useApp();
     const [title, setTitle] = react_1.useState("");
-    const [folder, setFolder] = react_1.useState("tutorials");
     const [selected, setSelected] = react_1.useState("");
     const createArticle = async () => {
         const route = slugify(title);
         const mdxPath = path_1.default.join(process.cwd(), `/${route}`);
         await fs_extra_1.default.ensureDir(mdxPath);
         await fs_extra_1.default.outputFile(`${mdxPath}/index.mdx`, mdxTemplate({ title }));
-        exit();
-    };
-    const syncArticles = async () => {
-        const dir = path_1.default.join(process.cwd(), `/${folder}`);
-        const files = await getMDXFiles(dir);
-        const paths = files.map((filePath) => {
-            return getPathParam(filePath, dir);
-        });
-        paths.forEach((pathParams) => {
-            const title = pathParams[0];
-            const pagePath = path_1.default.join(path_1.default.resolve(process.cwd(), "../pages/tutorials"), `${title}.tsx`);
-            if (!fs_extra_1.default.existsSync(pagePath)) {
-                const subPath = `${folder}/${pathParams.join("/")}.mdx`;
-                const contents = pageTemplate({ subPath });
-                fs_extra_1.default.writeFileSync(pagePath, contents);
-            }
-        });
         exit();
     };
     const handleSelect = (item) => {
@@ -129,20 +73,13 @@ const App = () => {
             label: "Create Tutorial",
             value: "create_tutorial",
         },
-        {
-            label: "Sync Tutorials",
-            value: "sync",
-        },
     ];
     return (react_1.default.createElement(ink_1.Box, null,
         !selected && react_1.default.createElement(ink_select_input_1.default, { items: items, onSelect: handleSelect }),
-        selected === "sync" && (react_1.default.createElement(ink_1.Text, { color: "green" },
-            react_1.default.createElement(ink_spinner_1.default, { type: "dots" }))),
         selected === "create_tutorial" && (react_1.default.createElement(ink_1.Box, null,
             react_1.default.createElement(ink_1.Box, { marginRight: 1 },
                 react_1.default.createElement(ink_1.Text, null, "Enter title of tutorial:")),
-            react_1.default.createElement(ink_text_input_1.default, { value: title, onChange: setTitle, onSubmit: createArticle }))),
-        selected === "sync" && (react_1.default.createElement(ink_text_input_1.default, { value: folder, onChange: setFolder, onSubmit: syncArticles }))));
+            react_1.default.createElement(ink_text_input_1.default, { value: title, onChange: setTitle, onSubmit: createArticle })))));
 };
 module.exports = App;
 exports.default = App;
--- source/ui.tsx
+++ source/ui.tsx
@@ -4,8 +4,7 @@ import { Box, Text, useApp } from "ink";
 import TextInput from "ink-text-input";
 import React, { useState } from "react";
 import SelectInput from "ink-select-input";
-import Spinner from "ink-spinner";
-import glob from "glob";
+
 const mdxTemplate = ({ title }: { title: string }) => {
 	return `
 import { REACT } from "../../../technologies";
@@ -23,36 +22,6 @@ export const meta = {
 `.trimStart();
 };
 
-const pageTemplate = ({ subPath }: { subPath: string }) => {
-	return `
-	import Article, { meta } from "../../content/${subPath}";
-	import Layout from "../../components/pages/layout";
-
-	const Tutorial = () => {
-		return (
-		  <Layout meta={meta}>
-			<Article />
-		  </Layout>
-		);
-	  };
-	  
-	  export default Tutorial;
-	`;
-};
-
-const getMDXFiles = (src: string): Promise<string[]> => {
-	return new Promise((resolve, reject) => {
-		glob(src + "/**/*.mdx", (err, res) => {
-			if (err) {
-				reject();
-				return;
-			}
-
-			resolve(res);
-		});
-	});
-};
-
 const slugify = (title: string = "") => {
 	const pieces = (title || "").match(/\S+\s*/g) ?? [];
 
@@ -66,21 +35,9 @@ const slugify = (title: string = "") => {
 	);
 };
 
-const getPathParam = (filePath: string, directory: string) => {
-	const bits = filePath
-		.replace(directory, "")
-		.split("/")
-		.filter(Boolean)
-		.reverse();
-	const [last, ...all] = bits ?? [];
-	//@ts-ignore
-	return [path.basename(last, path.extname(last)), ...all].reverse();
-};
-
 const App = () => {
 	const { exit } = useApp();
 	const [title, setTitle] = useState("");
-	const [folder, setFolder] = useState("tutorials");
 	const [selected, setSelected] = useState("");
 
 	const createArticle = async () => {
@@ -88,28 +45,6 @@ const App = () => {
 		const mdxPath = path.join(process.cwd(), `/${route}`);
 		await fs.ensureDir(mdxPath);
 		await fs.outputFile(`${mdxPath}/index.mdx`, mdxTemplate({ title }));
-		exit();
-	};
-	const syncArticles = async () => {
-		const dir = path.join(process.cwd(), `/${folder}`);
-		const files = await getMDXFiles(dir);
-		const paths = files.map((filePath) => {
-			return getPathParam(filePath, dir);
-		});
-
-		paths.forEach((pathParams) => {
-			const title = pathParams[0];
-			const pagePath = path.join(
-				path.resolve(process.cwd(), "../pages/tutorials"),
-				`${title}.tsx`
-			);
-
-			if (!fs.existsSync(pagePath)) {
-				const subPath = `${folder}/${pathParams.join("/")}.mdx`;
-				const contents = pageTemplate({ subPath });
-				fs.writeFileSync(pagePath, contents);
-			}
-		});
 		exit();
 	};
 
@@ -122,20 +57,11 @@ const App = () => {
 			label: "Create Tutorial",
 			value: "create_tutorial",
 		},
-		{
-			label: "Sync Tutorials",
-			value: "sync",
-		},
 	];
 
 	return (
 		<Box>
 			{!selected && <SelectInput items={items} onSelect={handleSelect} />}
-			{selected === "sync" && (
-				<Text color="green">
-					<Spinner type="dots" />
-				</Text>
-			)}
 			{selected === "create_tutorial" && (
 				<Box>
 					<Box marginRight={1}>
@@ -147,13 +73,6 @@ const App = () => {
 						onSubmit={createArticle}
 					/>
 				</Box>
-			)}
-			{selected === "sync" && (
-				<TextInput
-					value={folder}
-					onChange={setFolder}
-					onSubmit={syncArticles}
-				/>
 			)}
 		</Box>
 	);
